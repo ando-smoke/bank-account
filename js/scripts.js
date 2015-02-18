b9281c5b1dbc8879fb7e46ff205a67605d18221a
Andy Uppendahl and Kathryn Carr <kathrynelizabethcarr@gmail.com>
2015-02-18 10:40:33 -0800

    Refactor deposit and withdraw forms.

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,21 @@ var BankAccount = {
     this.balance -= amount;
   }
 };
+
+$(document).ready(function() {
+  $("form#new-account").submit(function(event) {
+    event.preventDefault();
+
+    var myBankAccount = Object.create(BankAccount);
+    myBankAccount.name = $("input#name").val();
+    myBankAccount.deposit(
+      parseFloat($("input#initial-deposit").val()));
+
+    $("#new-account").hide();
+    $("#deposit-form").show();
+    $("#withdraw-form").show();
+    $("#balance-field").show();
+
+    $(".balance").text(myBankAccount.balance);
+  });
+});
