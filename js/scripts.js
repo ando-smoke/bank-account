5bb22d1f4818fd564430b8d9982ce81b2a566d3c
Andy Uppendahl and Kathryn Carr <kathrynelizabethcarr@gmail.com>
2015-02-18 09:36:38 -0800

    Add withdraw method functionality to BankAccount prototype def with related specs.

--- js/scripts.js
+++ js/scripts.js
@@ -2,5 +2,8 @@ var BankAccount = {
   balance: 0.00,
   deposit: function(amount) {
     this.balance += amount;
+  },
+  withdraw: function(amount) {
+    this.balance -= amount;
   }
 };
--- spec/specs.js
+++ spec/specs.js
@@ -6,12 +6,20 @@ describe("BankAccount", function(){
     });
   });
 
-
   describe("deposit", function(){
     it("will add amount to BankAccount balance", function(){
       var testAccount = Object.create(BankAccount);
       testAccount.deposit(20.45);
       expect(testAccount.balance).to.equal(20.45);
+    });
+  });
+
+  describe("withdraw", function(){
+    it("will subtract amount from BankAccount balance", function(){
+      var testAccount = Object.create(BankAccount);
+      testAccount.deposit(30.50);
+      testAccount.withdraw(25.00);
+      expect(testAccount.balance).to.equal(5.50);
     });
   });
 });
